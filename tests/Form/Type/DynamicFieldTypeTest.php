1323f399a043c4f7b49567adcb0e9713d2797a13
Craig Heydenburg <craigh@mac.com>
2022-06-03 10:17:23 -0400

    add more tests refs #1

---
+++ tests/Form/Type/DynamicFieldTypeTest.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Zikula package.
+ *
+ * Copyright Zikula - https://ziku.la/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zikula\Bundle\DynamicFormPropertyBundle\Tests\Form\Type;
+
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\PreloadedExtension;
+use Symfony\Component\Form\Test\TypeTestCase;
+use Zikula\Bundle\DynamicFormPropertyBundle\Entity\AbstractDynamicPropertySpecification;
+use Zikula\Bundle\DynamicFormPropertyBundle\Form\Type\ChoiceTypeTransformed;
+use Zikula\Bundle\DynamicFormPropertyBundle\Form\Type\ChoiceWithOtherType;
+use Zikula\Bundle\DynamicFormPropertyBundle\Form\Type\DynamicFieldType;
+
+class DynamicFieldTypeTest extends TypeTestCase
+{
+    /**
+     * @return PreloadedExtension[]
+     */
+    protected function getExtensions(): array
+    {
+        $dispatcher = $this->createMock(EventDispatcherInterface::class);
+        $type = new DynamicFieldType($dispatcher, false);
+
+        return [
+            new PreloadedExtension([$type], []),
+        ];
+    }
+
+    /**
+     * @param string[] $expectedOptions
+     * @covers \Zikula\Bundle\DynamicFormPropertyBundle\Form\Type\DynamicFieldType
+     * @dataProvider data
+     */
+    public function testProperFormCreation(string $type, array $expectedOptions): void
+    {
+        $formData = new class() extends AbstractDynamicPropertySpecification {
+        };
+        $formData->setName('foo');
+        $formData->setFormType($type);
+        $form = $this->factory->create(DynamicFieldType::class, $formData);
+
+        // check basic types exist
+        $this->assertTrue($form->has('name'));
+        $this->assertTrue($form->has('formType'));
+        $this->assertTrue($form->has('labels'));
+        $this->assertTrue($form->has('active'));
+        $this->assertTrue($form->has('formOptions'));
+
+        // check types added by eventSubscribers exist
+        $expectedOptions = array_merge($expectedOptions, ['required', 'priority', 'help']); // standard options
+        foreach ($expectedOptions as $expectedField) {
+            $this->assertTrue($form->get('formOptions')->has($expectedField));
+        }
+        $this->assertCount(count($expectedOptions), $form->get('formOptions')); // no others exist
+
+        $this->assertTrue($form->isSynchronized());
+    }
+
+    public function data(): \Iterator
+    {
+        yield 'choice' => [ChoiceTypeTransformed::class, ['multiple', 'expanded', 'choices']];
+        yield 'choiceWithOther' => [ChoiceWithOtherType::class, ['multiple', 'expanded', 'choices']];
+        yield 'date' => [DateType::class, ['html5', 'widget', 'input', 'format', 'model_timezone']];
+        yield 'datetime' => [DateTimeType::class, ['html5', 'widget', 'input', 'format', 'model_timezone']];
+        yield 'time' => [TimeType::class, ['html5', 'widget', 'input', 'format', 'model_timezone']];
+        yield 'birthday' => [BirthdayType::class, ['html5', 'widget', 'input', 'format', 'model_timezone']];
+        yield 'money' => [MoneyType::class, ['currency']];
+        yield 'text' => [TextType::class, ['constraints']];
+        yield 'textarea' => [TextareaType::class, ['constraints']];
+        yield 'default' => [IntegerType::class, []];
+    }
+}
---
+++ tests/Form/Type/InlineFormDefinitionTypeTest.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Zikula package.
+ *
+ * Copyright Zikula - https://ziku.la/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zikula\Bundle\DynamicFormPropertyBundle\Tests\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormExtensionInterface;
+use Symfony\Component\Form\PreloadedExtension;
+use Symfony\Component\Form\Test\TypeTestCase;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Zikula\Bundle\DynamicFormPropertyBundle\Entity\AbstractDynamicPropertiesContainer;
+use Zikula\Bundle\DynamicFormPropertyBundle\Entity\AbstractDynamicPropertyData;
+use Zikula\Bundle\DynamicFormPropertyBundle\Entity\AbstractDynamicPropertySpecification;
+use Zikula\Bundle\DynamicFormPropertyBundle\Form\Type\ChoiceTypeTransformed;
+use Zikula\Bundle\DynamicFormPropertyBundle\Form\Type\InlineFormDefinitionType;
+
+class InlineFormDefinitionTypeTest extends TypeTestCase
+{
+    private TranslatorInterface $translator;
+
+    protected function setUp(): void
+    {
+        $this->translator = new class() implements TranslatorInterface {
+            /**
+             * @param string[] $parameters this is for phpstan
+             */
+            public function trans(string $id, array $parameters = [], string $domain = null, string $locale = null)
+            {
+                return $id;
+            }
+
+            public function getLocale(): string
+            {
+                return 'default';
+            }
+        };
+        parent::setUp();
+    }
+
+    /**
+     * @return FormExtensionInterface[]
+     */
+    protected function getExtensions(): array
+    {
+        $type = new InlineFormDefinitionType($this->translator);
+
+        return [
+            new PreloadedExtension([$type], []),
+        ];
+    }
+
+    /**
+     * @covers \Zikula\Bundle\DynamicFormPropertyBundle\Form\Type\InlineFormDefinitionType
+     */
+    public function testSubmitValidData(): void
+    {
+        $options = ['dynamicFieldsContainer' => new TempContainer()];
+        $result = new class() extends AbstractDynamicPropertyData {
+        };
+        $form = $this->factory->create(ParentFormType::class, $result, $options);
+        $this->assertTrue($form->get('fields')->has('name'));
+        $this->assertTrue($form->get('fields')->has('date'));
+        $this->assertTrue($form->get('fields')->has('color'));
+        $this->assertInstanceOf(TextType::class, $form->get('fields')->get('name')->getConfig()->getType()->getInnerType());
+        $this->assertInstanceOf(DateType::class, $form->get('fields')->get('date')->getConfig()->getType()->getInnerType());
+        $this->assertInstanceOf(ChoiceTypeTransformed::class, $form->get('fields')->get('color')->getConfig()->getType()->getInnerType());
+
+        $this->assertEmpty($result->getData());
+        $formData = ['fields' => ['name' => 'john', 'date' => '2022-01-01', 'color' => 'red']];
+        $form->submit($formData);
+        $this->assertTrue($form->isSynchronized());
+        $this->assertEquals($formData['fields'], $result->getData());
+    }
+}
+
+class ParentFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('fields', InlineFormDefinitionType::class, [
+                'dynamicFieldsContainer' => $options['dynamicFieldsContainer'],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setRequired('dynamicFieldsContainer');
+    }
+}
+
+class TempContainer extends AbstractDynamicPropertiesContainer
+{
+    /**
+     * @param array<string, mixed> $options
+     */
+    public function getSpec(string $name, string $type, array $options = []): AbstractDynamicPropertySpecification
+    {
+        $spec = new class() extends AbstractDynamicPropertySpecification {
+        };
+        $spec->setName($name);
+        $spec->setFormType($type);
+        $spec->setFormOptions($options);
+        $spec->setActive(true);
+
+        return $spec;
+    }
+
+    public function getPropertySpecifications(array $params = []): array
+    {
+        return [
+            $this->getSpec('name', TextType::class),
+            $this->getSpec('date', DateType::class, ['widget' => 'single_text', 'html5' => false, 'input' => 'string']),
+            $this->getSpec('color', ChoiceTypeTransformed::class, ['choices' => ['blue', 'red', 'yellow']]),
+        ];
+    }
+}

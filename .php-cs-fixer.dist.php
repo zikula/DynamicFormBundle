d4900fe78090206b4e454553502b76ad01cbed2a
Craig Heydenburg <craigh@mac.com>
2022-06-10 12:34:04 -0400

    cs improvements

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -7,6 +7,17 @@ $finder = (new PhpCsFixer\Finder())
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
+        'declare_strict_types' => true,
+        'header_comment' => [
+            'header' => 'This file is part of the Zikula package.
+
+Copyright Zikula - https://ziku.la/
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.',
+            'location' => 'after_declare_strict',
+        ],
     ])
+    ->setRiskyAllowed(true)
     ->setFinder($finder)
 ;
